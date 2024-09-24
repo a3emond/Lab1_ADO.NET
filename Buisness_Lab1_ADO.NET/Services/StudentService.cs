62b3eba944ea74d784b74bbf486351d5482f13c8
emond alexandre <a3emond@gmail.com>
2024-09-23 20:55:35 -0400

    basic buisness layer services filled

--- Buisness_Lab1_ADO.NET/Services/StudentService.cs
+++ Buisness_Lab1_ADO.NET/Services/StudentService.cs
@@ -1,34 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DAL_Lab1_ADO.NET.Models;
+using DAL_Lab1_ADO.NET.Repositories;
 
 namespace Buisness_Lab1_ADO.NET.Services
 {
     public class StudentService : Interfaces.IStudentService
     {
+        // use interface instead of concrete class to separate concerns
+        private readonly IRepository<Student> _studentRepository;
+
+        public StudentService(IRepository<Student> studentRepository)
+        {
+            _studentRepository = studentRepository ?? throw new ArgumentNullException(nameof(studentRepository));
+        }
         public void AddStudent(string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            // use the constructor without id to create a new instance of Student
+            var student = new Student(firstName, lastName);
         }
 
         public void DeleteStudent(int id)
         {
-            throw new NotImplementedException();
+            _studentRepository.Delete(id);
         }
 
         public List<Student> GetAllStudents()
         {
-            throw new NotImplementedException();
+            return _studentRepository.GetAll().ToList();
         }
 
         public Student GetStudentById(int id)
         {
-            throw new NotImplementedException();
+            return _studentRepository.GetById(id);
         }
 
         public void UpdateStudent(int id, string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            
+            _studentRepository.Update(new Student(_studentRepository.GetById(id).Id, firstName, lastName));
         }
     }
 }
